f51c0442c1c5bfdea6b4915f75fad8ca3ddfe4a5
Luke <a4010126@gmail.com>
2019-01-30 10:56:11 -0800

    Some little changes

--- project2/WebContent/index.js
+++ project2/WebContent/index.js
@@ -171,6 +171,13 @@ jQuery.ajax({
     success: (resultData) => handleStarResult(resultData) // Setting callback function to handle data returned successfully by the StarsServlet
 });*/
 
+/*
+jQuery.ajax({
+    dataType: "json", // Setting return data type
+    method: "GET", // Setting request method
+    url: "api/movies?p=" + currentPage + "&numRecord=" +recordNum+"&genre="+genre, // Setting request url, which is mapped by StarsServlet in Stars.java
+    success: (resultData) => handleStarResult(resultData) // Setting callback function to handle data returned successfully by the StarsServlet
+});*/
 
 jQuery.ajax({
     dataType: "json", // Setting return data type
